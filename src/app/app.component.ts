19daddda895dd05f25f8a6ef291d6c3a298a67bf
Larissa Duarte <larissaduarte93@gmail.com>
2021-12-20 16:05:14 -0400

    Implementando front

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import {FormControl, FormGroup} from '@angular/forms';
+import {TodoService} from './todo.service';
+import {Todo} from './todo';
+import {strict} from 'assert';
 
 @Component({
   selector: 'app-root',
@@ -11,7 +14,15 @@ export class AppComponent {
      description : new FormControl('')
    });
 
+   constructor(
+     private service: TodoService
+   ) {}
+
   submit() {
      console.log(this.form.value);
+     const todo: Todo = { ...this.form.value}
+     this.service
+       .salvar(todo)
+       .subscribe(savedTodo => console.log(savedTodo));
   }
 }
---
+++ src/app/todo.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import {Todo} from './todo';
+import {Observable} from 'rxjs';
+import {HttpClient} from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodoService {
+
+  apiUrl: string = 'http://localhost:8282/api/todos';
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  salvar(todo: Todo) : Observable<Todo> {
+    return this.http.post<Todo>(this.apiUrl, todo);
+  }
+}
